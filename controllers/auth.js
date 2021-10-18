2ba6c55ddbbf45f85bd6f0b9c32f888bd4c45115
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-17 22:18:50 -0400

    generate a token and send it in the response

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,18 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors/bad-request");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+
+  const token = await jwt.sign(
+    { userId: user._id, name: user.name },
+    "jwtSecret",
+    { expiresIn: "30d" }
+  );
+
+  res.status(StatusCodes.CREATED).json({ token });
 };
 
 const login = async (req, res) => {
