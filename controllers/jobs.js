990b5e5117858d34fc5cf9db1fb682e7922522f3
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-18 12:16:46 -0400

    set up update job controller and handler bad requests within

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -13,20 +13,43 @@ const getJob = async (req, res) => {
     params: { id: jobId },
   } = req;
   const job = await Job.findOne({ _id: jobId, createdBy: userId });
+
   if (!job) {
     throw new NotFoundError(`No job with id ${jobId}`);
   }
+
   res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
+  
   res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
-  res.send("update job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+    body: { company, position },
+  } = req;
+
+  if (company === " " || position === " ") {
+    throw new BadRequestError("Company and position cannot be empty");
+  }
+
+  const job = await Job.findOneAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const deleteJob = async (req, res) => {
